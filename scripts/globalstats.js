4b0977a4c3b551150ba63948429bad3560416fe4
ThomasNg0402 <s10259398@connect.np.edu.sg>
2024-01-25 23:48:47 +0800

    Updated locationstats.js&html and globalstats.js,html

--- scripts/globalstats.js
+++ scripts/globalstats.js
@@ -1,51 +1,56 @@
 // about.js
 $(document).ready(function () {
     const apiKey = "CwcyKOR5lUGs6RTfUXNeHw==AfTraDK9OjymV4LP";
-    var country = prompt("Enter country");
 
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/covid19?country=' + country,
-        headers: { 'X-Api-Key': apiKey },
-        contentType: 'application/json',
-        success: function (result) {
-            console.log(result);
+    // List of known countries
+    const countries = ["Afghanistan", "Albania", "Algeria", "Malaysia","Singapore"/* ... add more countries ... */];
 
-            // Display the result in a table
-            if (result && Array.isArray(result) && result.length > 0) {
-                var data = result[0]; // Assuming the data is the first element of the array
+    // Function to fetch COVID-19 data for a given country
+    function fetchData(country) {
+        return $.ajax({
+            method: 'GET',
+            url: 'https://api.api-ninjas.com/v1/covid19?country=' + country,
+            headers: { 'X-Api-Key': apiKey },
+            contentType: 'application/json'
+        });
+    }
 
-                if (data && data.cases) {
-                    var tableHtml = '<table class="table">';
-                    tableHtml += '<thead><tr><th>Date</th><th>Total Cases</th><th>New Cases</th></tr></thead>';
-                    tableHtml += '<tbody>';
+    // Function to display the result in a table
+    function displayTable(country, data) {
+        if (data && data.cases) {
+            var tableHtml = '<table class="table">';
+            tableHtml += '<thead><tr><th>Date</th><th>Total Cases</th><th>New Cases</th></tr></thead>';
+            tableHtml += '<tbody>';
 
-                    // Loop through the dates and add rows to the table
-                    for (var date in data.cases) {
-                        if (data.cases.hasOwnProperty(date)) {
-                            var dailyCases = data.cases[date];
-                            tableHtml += '<tr><td>' + date + '</td><td>' + dailyCases.total + '</td><td>' + dailyCases.new + '</td></tr>';
-                        }
-                    }
-
-                    tableHtml += '</tbody></table>';
-
-                    // Append the table to the result-container div
-                    
-                    $('.result-container').html(tableHtml);
-                } else {
-                    // Display a message if there is no cases data
-                    $('.result-container').html('<p>No cases data available</p>');
+            // Loop through the dates and add rows to the table
+            for (var date in data.cases) {
+                if (data.cases.hasOwnProperty(date)) {
+                    var dailyCases = data.cases[date];
+                    tableHtml += '<tr><td>' + date + '</td><td>' + dailyCases.total + '</td><td>' + dailyCases.new + '</td></tr>';
                 }
-            } else {
-                // Display a message if there is no data
-                $('.result-container').html('<p>No data available</p>');
             }
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-            // Display the error in the result-container div
-            $('.result-container').html('<p>Error: ' + jqXHR.responseText + '</p>');
+
+            tableHtml += '</tbody></table>';
+
+            // Append the table to the result-container div
+            $('.result-container').append('<h2>' + country + ' Cases</h2>').append(tableHtml);
+        } else {
+            // Display a message if there is no cases data
+            $('.result-container').append('<p>No cases data available for ' + country + '</p>');
         }
+    }
+
+    // Loop through each country and fetch data
+    countries.forEach(function (country) {
+        fetchData(country)
+            .done(function (result) {
+                console.log(result);
+                displayTable(country, result[0]); // Assuming the data is the first element of the array
+            })
+            .fail(function (jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+                // Display the error in the result-container div
+                $('.result-container').append('<p>Error fetching data for ' + country + ': ' + jqXHR.responseText + '</p>');
+            });
     });
 });
